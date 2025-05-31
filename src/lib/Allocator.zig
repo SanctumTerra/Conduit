0bcdd70eaf6e172ea07e4047182e407933041bf1
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-05-31 12:42:22 +0300

    Allocator

---
+++ src/lib/Allocator.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const heap = std.heap;
+
+var allocator = heap.GeneralPurposeAllocator(.{ .safety = true, .enable_memory_limit = true }){};
+
+pub fn get() std.mem.Allocator {
+    return allocator.allocator();
+}
+
+/// Returns the memory usage in MB
+pub fn getMemoryUsage() void {
+    const bytes = allocator.total_requested_bytes;
+    const mb = bytes / (1024 * 1024);
+    std.debug.print("Memory usage: {} MB\n", .{mb});
+}
+
+pub fn deinit() std.heap.Check {
+    return allocator.deinit();
+}
