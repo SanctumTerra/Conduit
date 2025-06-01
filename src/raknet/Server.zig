ed03af8ce8d62439989189652c352e8d633bf473
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-06-01 23:03:06 +0300

    Add connection handling in Server struct, including methods for processing OpenConnectionRequest1 and OpenConnectionRequest2 packets, and implement address key generation for connections.

--- src/raknet/Server.zig
+++ src/raknet/Server.zig
@@ -5,8 +5,18 @@ const Callocator = @import("CAllocator");
 const Packets = @import("./proto/Packets.zig").Packets;
 const UnconnectedPing = @import("./proto/offline/UnconnectedPing.zig").UnconnectedPing;
 const UnconnectedPong = @import("./proto/offline/UnconnectedPong.zig").UnconnectedPong;
+const Address = @import("./proto/Address.zig").Address;
 const Advertisement = @import("./proto/Advertisement.zig").Advertisement;
+const ConnectionRequest1 = @import("./proto/offline/ConnectionRequest1.zig").ConnectionRequest1;
+const ConnectionReply1 = @import("./proto/offline/ConnectionReply1.zig").ConnectionReply1;
+const ConnectionRequest2 = @import("./proto/offline/ConnectionRequest2.zig").ConnectionRequest2;
+const ConnectionReply2 = @import("./proto/offline/ConnectionReply2.zig").ConnectionReply2;
 
+const Connection = @import("./Connection.zig").Connection;
+
+pub const UDP_HEADER_SIZE: u16 = 28;
+pub const MTU_SIZES = [_]u16{ 1492, 1200, 576 };
+pub const MAX_MTU_SIZE: u16 = 1492;
 const ServerConfig = struct {
     const Self = @This();
     address: []const u8 = "0.0.0.0",
@@ -35,11 +45,10 @@ pub const Server = struct {
     const Self = @This();
     config: ServerConfig,
     socket: ?Socket,
+    connections: std.StringHashMap(Connection),
     allocator: std.mem.Allocator,
-    // var prng = std.Random.DefaultPrng.init(@as(u64, @intCast(std.time.milliTimestamp())));
-    // const random_guid = prng.random().int(i64);
     pub fn init(config: ServerConfig) !Self {
-        return Self{ .config = config, .socket = null, .allocator = Callocator.get() };
+        return Self{ .config = config, .socket = null, .allocator = Callocator.get(), .connections = std.StringHashMap(Connection).init(Callocator.get()) };
     }
 
     pub fn deinit(self: *Self) void {
@@ -70,14 +79,37 @@ pub const Server = struct {
         defer {
             // Log data length
             Callocator.get().free(data);
-            Logger.INFO("| {any} | ", .{data.len});
+            // Logger.INFO("| {any} | ", .{data.len});
         }
-
         server.handlePacket(data, from_addr);
     }
 
+    pub fn getAddressAsKey(self: *Self, address: std.net.Address) []const u8 {
+        _ = self;
+        const key_address: Address = Address.initFromRawBuiltin(
+            &address.any,
+            address.getPort(),
+            @as(u8, @intCast(address.any.family)),
+            Callocator.get(),
+        ) catch |err| {
+            Logger.ERROR("Failed to initialize address: {}", .{err});
+            return &[_]u8{};
+        };
+        const key = std.fmt.allocPrint(Callocator.get(), "{s}-{d}", .{ key_address.address, key_address.port }) catch |err| {
+            Logger.ERROR("Failed to allocate key: {}", .{err});
+            return &[_]u8{};
+        };
+        // defer Callocator.get().free(key);
+        defer key_address.deinit(Callocator.get());
+        return key;
+    }
+
     pub fn handlePacket(self: *Self, data: []const u8, from_addr: std.net.Address) void {
-        const ID = data[0];
+        var ID: u8 = data[0];
+        if (ID & 0xF0 == 0x80) ID = 0x80;
+        const key = self.getAddressAsKey(from_addr);
+        defer Callocator.get().free(key);
+        Logger.INFO("Key: {s}", .{key});
 
         switch (ID) {
             Packets.UnconnectedPing => {
@@ -92,6 +124,34 @@ pub const Server = struct {
                 self.send(pong_data, from_addr);
                 Callocator.get().free(string);
                 defer Callocator.get().free(pong_data);
+            },
+            Packets.OpenConnectionRequest1 => {
+                // const request = ConnectionRequest1.deserialize(data);
+                // NOTE! No need to free this, as its not allocated.
+                var reply = ConnectionReply1.init(
+                    self.config.advertisement.guid,
+                    false,
+                    1492,
+                );
+                const reply_data = reply.serialize();
+                self.send(reply_data, from_addr);
+                defer Callocator.get().free(reply_data);
+            },
+            Packets.OpenConnectionRequest2 => {
+                const request = ConnectionRequest2.deserialize(data);
+                Logger.INFO("Connection request 2: {any}", .{request});
+                defer request.address.deinit(Callocator.get());
+                const address = Address.init(4, "0.0.0.0", 0);
+                var reply = ConnectionReply2.init(
+                    self.config.advertisement.guid,
+                    address,
+                    1492,
+                    false,
+                );
+                const reply_data = reply.serialize();
+                self.send(reply_data, from_addr);
+                defer Callocator.get().free(reply_data);
+                // defer address.deinit(Callocator.get());
             },
             0x0e => {
                 // Skip, test packet
