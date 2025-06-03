cd7bce15497809f3280849a77016967743e99007
AnyBananaGAME <ainaraspirmas@gmail.com>
2025-06-03 22:57:42 +0300

    Refactor server initialization in main.zig to use the new conduit.Server module. Update server start method and configuration options, including address, port, max players, and tick rate for improved server management.

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,20 @@ const Callocator = @import("CAllocator");
 const Logger = @import("Logger").Logger;
 const BinaryStream = @import("BinaryStream");
 const Socket = @import("./raknet/socket/socket.zig").Socket;
-const Server = @import("./raknet/Server.zig").Server;
+const Server = @import("./conduit/Server.zig").Server;
 
 pub fn main() !void {
     defer _ = Callocator.deinit();
 
-    var server = try Server.init(.{
+    var server = try Server.init(Callocator.get(), .{
+        .address = "0.0.0.0",
         .port = 19132,
+        .max_players = 60,
+        .tick_rate = 20,
     });
     defer server.deinit();
 
-    server.listen();
+    try server.start();
 
     while (true) {
         Callocator.getMemoryUsage();
