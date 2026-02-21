e307f1bbea2fdf0ee4ac8d2118ea58e4e6aaed6c
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-02-21 18:43:57 +0200

    Add client data and disconnect to player struct

--- src/conduit/player/player.zig
+++ src/conduit/player/player.zig
@@ -1,25 +1,46 @@
 const std = @import("std");
 const Raknet = @import("Raknet");
+const LoginData = @import("protocol").Login.Decoder.LoginData;
+pub const NetworkHandler = @import("../network/network-handler.zig").NetworkHandler;
 
 pub const Player = struct {
-    connection: *Raknet.Connection,
     allocator: std.mem.Allocator,
-
+    connection: *Raknet.Connection,
+    network: *NetworkHandler,
+    loginData: LoginData,
     runtimeId: i64,
+
+    xuid: []const u8,
+    username: []const u8,
+    uuid: []const u8,
 
     pub fn init(
         allocator: std.mem.Allocator,
         connection: *Raknet.Connection,
+        network: *NetworkHandler,
+        loginData: LoginData,
         runtimeId: i64,
     ) !Player {
         return Player{
             .allocator = allocator,
             .connection = connection,
+            .network = network,
+            .loginData = loginData,
             .runtimeId = runtimeId,
+            .xuid = loginData.identity_data.xuid,
+            .username = loginData.identity_data.display_name,
+            .uuid = loginData.identity_data.identity,
         };
     }
 
     pub fn deinit(self: *Player) void {
-        _ = self;
+        self.loginData.deinit();
+    }
+
+    pub fn disconnect(self: *Player) !void {
+        if (self.network.conduit.players.fetchRemove(self.runtimeId)) |entry| {
+            entry.value.deinit();
+            self.allocator.destroy(entry.value);
+        }
     }
 };
--- src/protocol/root.zig
+++ src/protocol/root.zig
@@ -5,16 +5,20 @@ pub const Packet = @import("./enums/packet.zig").Packet;
 pub const CompressionMethod = @import("./enums/compression-method.zig").CompressionMethod;
 pub const DisconnectReason = @import("./enums/disconnect-reason.zig").DisconnectReason;
 pub const PlayStatusEnum = @import("./enums/play-status.zig").PlayStatus;
+pub const ResourcePackResponse = @import("./enums/resource-pack-response.zig").ResourcePackResponse;
 
 pub const Experiments = @import("./types/experiments.zig").Experiments;
 pub const ResourceIdVersions = @import("./types/resource-id-versions.zig").ResourceIdVersions;
 pub const ResourcePackDescriptor = @import("./types/resource-pack-descriptor.zig").ResourcePackDescriptor;
 pub const Uuid = @import("./types/uuid.zig").Uuid;
+pub const ResourcePacksClientRequest = @import("./types/resource-packs-client-request.zig").ResourcePacksClientRequest;
+pub const RequestedResourcePack = @import("./types/requested-resource-pack.zig").RequestedResourcePack;
 
 pub const RequestNetworkSettingsPacket = @import("./packets/request-network-settings.zig").RequestNetworkSettings;
 pub const NetworkSettingsPacket = @import("./packets/network-settings.zig").NetworkSettings;
 pub const LoginPacket = @import("./packets/login.zig").Login;
 pub const DisconnectPacket = @import("./packets/disconnect.zig").Disconnect;
 pub const PlayStatusPacket = @import("./packets/play-status.zig").PlayStatus;
-pub const ResourcePacksStackPacket = @import("./packets/resource-pack-stack.zig").ResourcePackStackPacket;
+pub const ResourcePackStackPacket = @import("./packets/resource-pack-stack.zig").ResourcePackStackPacket;
 pub const ResourcePacksInfoPacket = @import("./packets/resource-packs-info.zig").ResourcePacksInfoPacket;
+pub const ResourcePackResponsePacket = @import("./packets/resource-pack-response.zig").ResourcePackClientResponsePacket;
