621a89c6831a4ab27e32b6c86fb836db641ec1e6
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-02-21 17:09:56 +0200

    Exports

--- src/protocol/root.zig
+++ src/protocol/root.zig
@@ -1,5 +1,20 @@
+pub const PROTOCOL = 924;
+pub const Login = @import("./login/root.zig");
+
 pub const Packet = @import("./enums/packet.zig").Packet;
 pub const CompressionMethod = @import("./enums/compression-method.zig").CompressionMethod;
+pub const DisconnectReason = @import("./enums/disconnect-reason.zig").DisconnectReason;
+pub const PlayStatusEnum = @import("./enums/play-status.zig").PlayStatus;
+
+pub const Experiments = @import("./types/experiments.zig").Experiments;
+pub const ResourceIdVersions = @import("./types/resource-id-versions.zig").ResourceIdVersions;
+pub const ResourcePackDescriptor = @import("./types/resource-pack-descriptor.zig").ResourcePackDescriptor;
+pub const Uuid = @import("./types/uuid.zig").Uuid;
 
 pub const RequestNetworkSettingsPacket = @import("./packets/request-network-settings.zig").RequestNetworkSettings;
 pub const NetworkSettingsPacket = @import("./packets/network-settings.zig").NetworkSettings;
+pub const LoginPacket = @import("./packets/login.zig").Login;
+pub const DisconnectPacket = @import("./packets/disconnect.zig").Disconnect;
+pub const PlayStatusPacket = @import("./packets/play-status.zig").PlayStatus;
+pub const ResourcePacksStackPacket = @import("./packets/resource-pack-stack.zig").ResourcePackStackPacket;
+pub const ResourcePacksInfoPacket = @import("./packets/resource-packs-info.zig").ResourcePacksInfoPacket;
