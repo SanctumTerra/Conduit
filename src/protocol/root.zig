e323ccb5bad41fff8fb0b4c6bb8a1e09ba101497
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-02-21 19:04:40 +0200

    Implement StartGame packet

--- src/protocol/root.zig
+++ src/protocol/root.zig
@@ -6,6 +6,9 @@ pub const CompressionMethod = @import("./enums/compression-method.zig").Compress
 pub const DisconnectReason = @import("./enums/disconnect-reason.zig").DisconnectReason;
 pub const PlayStatusEnum = @import("./enums/play-status.zig").PlayStatus;
 pub const ResourcePackResponse = @import("./enums/resource-pack-response.zig").ResourcePackResponse;
+pub const GameMode = @import("./enums/gamemode.zig").Gamemode;
+pub const PermissionLevel = @import("./enums/permission-level.zig").PermissionLevel;
+pub const Difficulty = @import("./enums/difficulty.zig").Difficulty;
 
 pub const Experiments = @import("./types/experiments.zig").Experiments;
 pub const ResourceIdVersions = @import("./types/resource-id-versions.zig").ResourceIdVersions;
@@ -13,6 +16,12 @@ pub const ResourcePackDescriptor = @import("./types/resource-pack-descriptor.zig
 pub const Uuid = @import("./types/uuid.zig").Uuid;
 pub const ResourcePacksClientRequest = @import("./types/resource-packs-client-request.zig").ResourcePacksClientRequest;
 pub const RequestedResourcePack = @import("./types/requested-resource-pack.zig").RequestedResourcePack;
+pub const ServerTelemetryData = @import("./types/server-telemetry-data.zig").ServerTelemetryData;
+pub const Vector3f = @import("./types/vector3f.zig").Vector3f;
+pub const Vector2f = @import("./types/vector2f.zig").Vector2f;
+pub const Gamerules = @import("./types/game-rules.zig");
+pub const BlockPosition = @import("./types/block-position.zig").BlockPosition;
+pub const NetworkBlockTypeDefinition = @import("./types/network-block-type-definition.zig").NetworkBlockTypeDefinition;
 
 pub const RequestNetworkSettingsPacket = @import("./packets/request-network-settings.zig").RequestNetworkSettings;
 pub const NetworkSettingsPacket = @import("./packets/network-settings.zig").NetworkSettings;
