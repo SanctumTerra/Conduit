ab404371681032acf9ff54b45d449936600915fd
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-02-23 18:57:52 +0200

    Fix Animate handler

--- src/conduit/network/handlers/animate.zig
+++ src/conduit/network/handlers/animate.zig
@@ -2,7 +2,6 @@ const Raknet = @import("Raknet");
 const NetworkHandler = @import("../network-handler.zig").NetworkHandler;
 const BinaryStream = @import("BinaryStream").BinaryStream;
 const Protocol = @import("protocol");
-const Player = @import("../../player/player.zig").Player;
 
 pub fn handleAnimate(
     network: *NetworkHandler,
@@ -17,7 +16,7 @@ pub fn handleAnimate(
 
     const out_packet = Protocol.AnimatePacket{
         .action = packet.action,
-        .runtime_entity_id = @intCast(player.runtimeId),
+        .runtime_entity_id = @intCast(player.entity.runtime_id),
         .data = packet.data,
         .swing_source = packet.swing_source,
     };
@@ -25,7 +24,7 @@ pub fn handleAnimate(
 
     const snapshots = network.conduit.getPlayerSnapshots();
     for (snapshots) |other| {
-        if (other.runtimeId == player.runtimeId) continue;
+        if (other.entity.runtime_id == player.entity.runtime_id) continue;
         if (!other.spawned) continue;
         try network.sendPacket(other.connection, serialized);
     }
